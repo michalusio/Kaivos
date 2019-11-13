0910361a94d6fcf601401cde437caa9da52b3a53
michalusio <micha_i@interia.pl>
2019-11-13 20:59:41 +0100

    Set up MainCamera correctly: Unity is stupid

--- build-master.bat
+++ build-master.bat
@@ -3,13 +3,14 @@ REM S2Fpdm9zU3VwZXJHcmE
 rd /s /q Kaivos-Master-Branch
 cd /D %temp%
 git config --list | findstr /C:"filter.lfs.smudge" 1>nul
-if errorlevel 1 (
+IF errorlevel 1 (
 	echo. GIT-LFS not found, falling back to normal _clone_
 	git clone --single-branch --no-hardlinks https://github.com/michalusio/Kaivos.git Kaivos-Master-Branch
 ) ELSE (
+	echo. GIT-LFS found, download will be quick
 	git lfs clone --single-branch --no-hardlinks https://github.com/michalusio/Kaivos.git Kaivos-Master-Branch
 )
 md Kaivos-Master-Build
-"D:\Programy\UnityEditors\2019.2.12f1\Editor\Unity.exe" -batchmode -projectPath ".\Kaivos-Master-Branch\" -logfile "./Kaivos-Master-Build/KaivosBuild.log" -silent-crashes -nographics -executeMethod ScriptBatch.BuildGame -quit
+"D:\Programy\UnityEditors\2019.2.12f1\Editor\Unity.exe" -batchmode -projectPath ".\Kaivos-Master-Branch\" -logfile "./Kaivos-Master-Build/KaivosBuild.log" -silent-crashes -executeMethod ScriptBatch.BuildGame -quit
 rd /s /q Kaivos-Master-Branch
 start Kaivos-Master-Build
