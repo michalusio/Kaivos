4485e45f450a646dd544a1134e22e3698a2fb914
michalusio <micha_i@interia.pl>
2019-11-13 21:10:18 +0100

    Changed build directory to PATH/BUILD/

--- build-master.bat
+++ build-master.bat
@@ -4,12 +4,13 @@ rd /s /q Kaivos-Master-Branch
 cd /D %temp%
 git config --list | findstr /C:"filter.lfs.smudge" 1>nul
 IF errorlevel 1 (
-	echo. GIT-LFS not found, falling back to normal _clone_
+	echo.GIT-LFS not found, falling back to normal _clone_
 	git clone --single-branch --no-hardlinks https://github.com/michalusio/Kaivos.git Kaivos-Master-Branch
 ) ELSE (
-	echo. GIT-LFS found, download will be quick
+	echo.GIT-LFS found, download should be quick
 	git lfs clone --single-branch --no-hardlinks https://github.com/michalusio/Kaivos.git Kaivos-Master-Branch
 )
+echo.Building Kaivos using Unity...
 md Kaivos-Master-Build
 "D:\Programy\UnityEditors\2019.2.12f1\Editor\Unity.exe" -batchmode -projectPath ".\Kaivos-Master-Branch\" -logfile "./Kaivos-Master-Build/KaivosBuild.log" -silent-crashes -executeMethod ScriptBatch.BuildGame -quit
 rd /s /q Kaivos-Master-Branch
