899977f34e929149f6342abf3b1538c1f098841f
michalusio <micha_i@interia.pl>
2019-12-04 19:21:21 +0100

    Added new tree textures. Added forge and junction. Added block shadow. Rewritten inventory script.

--- Assets/Scripts/Feature Scripts/DrawingScript.cs
+++ Assets/Scripts/Feature Scripts/DrawingScript.cs
@@ -1,24 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(MainScript), typeof(CharacterMovementScript), typeof(MiningScript))]
 public class DrawingScript : MonoBehaviour
 {
     public Camera GuiCamera;
     public Material TileSetMapMaterial;
+    public RawImage SlotCursor;
 
     private MainScript _mainScript;
     private CharacterMovementScript _characterMovementScript;
     private MiningScript _miningScript;
+    private InventoryScript _inventoryScript;
+
+    private (int, int) prevInvSlot;
 
     void Start()
     {
         _mainScript = GetComponent<MainScript>();
         _characterMovementScript = GetComponent<CharacterMovementScript>();
         _miningScript = GetComponent<MiningScript>();
+        _inventoryScript = GetComponent<InventoryScript>();
         var tileTexture = TileSetMapMaterial.GetTexture("_TileTex");
         var machineTexture = TileSetMapMaterial.GetTexture("_MachineTex");
         TileSetMapMaterial.SetVector("_Sizes", new Vector4(_mainScript.mainTexture.width, _mainScript.mainTexture.height, tileTexture.width, tileTexture.height));
         TileSetMapMaterial.SetVector("_Sizes2", new Vector4(machineTexture.width, machineTexture.height, 0, 0));
+
+        UpdateInventorySlot();
     }
 
     void OnGUI()
@@ -36,6 +44,8 @@ public class DrawingScript : MonoBehaviour
             DrawCharacter(mapScale, screenHalfSize);
 
             DrawMouseTilePointer(screenHalfSize, mapScale, position, mapScaledHalfSize);
+
+            DrawMouseTileGhost(screenHalfSize, mapScale, position, mapScaledHalfSize);
 
             DebugCollisionBoxShow();
 
@@ -69,6 +79,43 @@ public class DrawingScript : MonoBehaviour
         _miningScript.MineTileMaterial.SetInt("_Size", _miningScript.ChosenMineSize * 2 - 1);
         var mineHalfSize = new Vector2(_miningScript.ChosenMineSize - 1, _miningScript.ChosenMineSize - 1);
         Graphics.DrawTexture(new Rect(t * mapScale + (position - mineHalfSize) * mapScale - mapScaledHalfSize + screenHalfSize, tileSize * (_miningScript.ChosenMineSize * 2 - 1)), _miningScript.MineTileTexture, _miningScript.MineTileMaterial);
+    }
+
+    private void DrawMouseTileGhost(Vector2 screenHalfSize, int mapScale, Vector2 position, Vector2 mapScaledHalfSize)
+    {
+        if (_inventoryScript.ChosenSlot.Tab < 0)
+        {
+            SlotCursor.gameObject.SetActive(false);
+            return;
+        }
+        
+        SlotCursor.gameObject.SetActive(true);
+        if (prevInvSlot != _inventoryScript.ChosenSlot)
+        {
+            UpdateInventorySlot();
+        }
+
+        var blockSize = new Vector2(1, 1);
+        if (prevInvSlot.Item1 == 1) blockSize = new Vector2(3, 3);
+
+        var mouseP = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+
+        var t = mouseP - screenHalfSize + mapScaledHalfSize - position * mapScale;
+        t /= mapScale;
+        t = new Vector2(Mathf.Floor(t.x), Mathf.Floor(t.y));
+        
+        var (Tab, Slot) = _inventoryScript.ChosenSlot;
+        var anchPos = t * mapScale + (position - blockSize * 0.5f) * mapScale - mapScaledHalfSize;
+        SlotCursor.GetComponent<RectTransform>().anchoredPosition = new Vector2(anchPos.x + mapScale * blockSize.x * 0.5f + mapScale * 0.5f, -anchPos.y - mapScale * blockSize.y * 0.5f - mapScale * 0.5f);
+        SlotCursor.GetComponent<RectTransform>().sizeDelta = new Vector2(mapScale, mapScale) * blockSize;
+    }
+
+    private void UpdateInventorySlot()
+    {
+        prevInvSlot = _inventoryScript.ChosenSlot;
+        SlotCursor.material = Instantiate(_inventoryScript.BlockSlots[prevInvSlot.Item1][prevInvSlot.Item2].material);
+        SlotCursor.texture = _inventoryScript.BlockSlots[prevInvSlot.Item1][prevInvSlot.Item2].mainTexture;
+        SlotCursor.uvRect = _inventoryScript.BlockSlots[prevInvSlot.Item1][prevInvSlot.Item2].uvRect;
     }
 
     private void DebugCollisionBoxShow()
---
+++ Assets/Scripts/UI Scripts/MenuMachineItemSpriteSet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using MachineInfo = System.ValueTuple<int, int, int, int>;
+
+[ExecuteAlways]
+public class MenuMachineItemSpriteSet : MonoBehaviour
+{
+    
+
+    public MachineType SetTo;
+
+    public enum MachineType
+    {
+        SHOP,
+        FORGE,
+        JUNCTION
+    }
+
+    private static readonly Dictionary<MachineType, MachineInfo> MachineTypeParams = new Dictionary<MachineType, MachineInfo>
+    {
+        {MachineType.SHOP, (0, 12, 15, 5)},
+        {MachineType.JUNCTION, (20, 12, 12, 8)},
+        {MachineType.FORGE, (32, 12, 12, 5)},
+    };
+
+    void Start()
+    {
+        var drawingScript = FindObjectOfType<DrawingScript>();
+        var image = GetComponent<RawImage>();
+        var tileTexture = drawingScript.TileSetMapMaterial.GetTexture("_MachineTex");
+        var machineParams = MachineTypeParams[SetTo];
+        image.uvRect = new Rect(0, (tileTexture.height - machineParams.Item3 - (float)machineParams.Item1)/tileTexture.height, machineParams.Item2/(float)tileTexture.width, machineParams.Item3/(float)tileTexture.height);
+        
+        var material = Instantiate(image.material);
+        material.SetInt("_AnimationSize", machineParams.Item4);
+        image.material = material;
+    }
+
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            Start();
+        }
+    }
+}
+
